4d22e398dfa8cf9c6ba2c88efcef6728364741d6
JavierLlanos6 <xavi6poop@gmail.com>
2021-10-17 20:05:31 -0400

    arreglado de redireccion a una leccion

--- src/components/Tab1.js
+++ src/components/Tab1.js
@@ -11,7 +11,7 @@ function Tab1() {
             <pre className = {styles.temas}>          SUMA +          RESTA -          MULTIPLICACION x          DIVISION %</pre>
             <button className = {styles.boton1} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 1 +
@@ -25,98 +25,98 @@ function Tab1() {
             </button>
             <button className = {styles.boton3} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 3 +
             </button>
             <button className = {styles.boton4} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 4 +
             </button>
             <button className = {styles.boton5} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 1 -
             </button>
             <button className = {styles.boton6} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 2 -
             </button>
             <button className = {styles.boton7} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 3 -
             </button>
             <button className = {styles.boton8} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 4 -
             </button>
             <button className = {styles.boton9} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 1 x
             </button>
             <button className = {styles.boton10} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 2 x
             </button>
             <button className = {styles.boton11} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 3 x
             </button>
             <button className = {styles.boton12} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 4 x
             </button>
             <button className = {styles.boton13} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 1 %
             </button>
             <button className = {styles.boton14} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 2 %
             </button>
             <button className = {styles.boton15} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 3 %
             </button>
             <button className = {styles.boton16} onClick ={
                 ()=>{
-                    history.push("/Tab2");
+                    history.push("/Leccion1");
                 }
             }>
                 Leccion 4 %
